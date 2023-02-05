0dcd9017f569739cc8966c8260be27dd00618c04
emilamata <emilamata@gmail.com>
2023-02-05 05:24:08 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from typing import Union
+from fastapi import FastAPI
+
+# Creacíon de una aplicacíon FastAPI:
+app = FastAPI()
+
+@app.get('/')
+def read_root():
+    return {"Hello": "World!!"}
+
+
+@app.get('/items/{item_id}')
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {'item_id': item_id, 'q' : q}
+
+
+@app.get('/calculadora')
+def calcular(operando_1:float, operando_2:float):
+    return {'suma': operando_1 + operando_2}
