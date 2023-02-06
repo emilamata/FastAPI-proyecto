dd621d0b039dfd417b6b5d303fbbe4d4f64ebcf9
emilamata <emilamata@gmail.com>
2023-02-06 02:48:36 -0300

    creado el modelo Item

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # Creacíon de una aplicacíon FastAPI:
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 @app.get('/')
 def read_root():
     return {"Hello": "World!!"}
 
+@app.get('/Hola')
+def read_root():
+    return {"Hello": "World!!"}
 
 @app.get('/items/{item_id}')
 def read_item(item_id: int, q: Union[str, None] = None):
